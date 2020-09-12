fa6bcafdde54158bad977e5efce58b00b104f56b
funblaster22 <fogogrl@hotmail.com>
2020-09-12 12:39:18 -0500

    Family code signup duplicate prevention

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+export const post = async (url, data) => (await fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: JSON.stringify(data) // body data type must match "Content-Type" header
+})).json();
